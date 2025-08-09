5bc6381371f16f62a826fba16571d0bb02c6a36a
HellPortal <104918285+HellPortal@users.noreply.github.com>
2025-08-09 02:20:57 -0400

    Update fxmanifest.lua

--- hp_shop247/fxmanifest.lua
+++ hp_shop247/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'HellPortal'
-description 'Retextured, Static Door Deleted, New Door Added, Floor Dirt Cleaned and Reflective Floor Added'
+description 'Shop 24/7 Retextured, Static Door Deleted, New Door Added, Floor Dirt Cleaned and Reflective Floor Added'
 version '1.0.0'
 
 this_is_a_map 'yes'
